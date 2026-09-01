7e864722ad4b534b0ce67d070578b4242886e30e
Guilherme Augusto <guilherme.cavalcante78@gmail.com>
2026-09-01 02:08:32 -0300

    fix AI Provider

--- tests/OrganizadorDocumentos.Tests/obj/Debug/net10.0/OrganizadorDocumentos.Tests.AssemblyInfo.cs
+++ tests/OrganizadorDocumentos.Tests/obj/Debug/net10.0/OrganizadorDocumentos.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrganizadorDocumentos.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef4ef916196ee98c551afd892de9e65dd1da5a6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrganizadorDocumentos.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrganizadorDocumentos.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
